0634db593e4ad9a41adb90b771422bccba245c2e
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-11 15:20:43 +0000

    Rename and Unit Test for the former CollectionWritable class.

---
+++ tests/pt/um/mrc/util/datatypes/ArrayWritablePrintableTest.java
@@ -0,0 +1,48 @@
+package pt.um.mrc.util.datatypes;
+
+import static org.junit.Assert.*;
+
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Writable;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayWritablePrintableTest {
+	
+	ArrayWritablePrintable awp;
+	String[] strings;
+
+	@Before
+	public void setUp() throws Exception {	
+		strings = new String[2];
+		strings[0]="foo";
+		strings[1]="bar";
+		awp = new ArrayWritablePrintable(strings);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testArrayWritablePrintable() {
+		ArrayWritablePrintable awpToTest = new ArrayWritablePrintable(strings);
+		ArrayWritable aux = new ArrayWritable(strings);
+		
+		Writable[] expected = aux.get();
+		Writable[] actual = awpToTest.get();
+		
+		assertArrayEquals(expected, actual);
+		}
+
+	@Test
+	public void testToString() {
+		String expected = "{ foo bar}";
+		String actual = awp.toString();
+		assertEquals(expected, actual);
+	}
+
+}
