0b3541df325c9374fc9c777c3347e260c1fb2030
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-11 14:51:09 +0000

    Unit test for the IndentityReducer

---
+++ trunk/tests/pt/um/mrc/util/reducers/IdentityReducerTest.java
@@ -0,0 +1,36 @@
+package pt.um.mrc.util.reducers;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+
+public class IdentityReducerTest {
+
+	private Reducer<Text,Text,Text,Text> reducer;
+	private ReduceDriver<Text,Text,Text,Text> driver;
+	private List<Text> vis = new ArrayList<Text>();
+	private List<Text> vos = new ArrayList<Text>();
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		reducer = new Reducer<Text,Text,Text,Text>();
+		driver  = new ReduceDriver<Text,Text,Text,Text>(reducer);
+		vis.add(new Text("bar"));
+		vos.add(new Text("bar"));
+	}
+
+
+	@Test
+	public void testReduceKIIterableOfVIContext() {
+		driver.withInput(new Text("foo"), vis).withOutput(new Text("foo"), new Text("bar")).runTest();
+
+	}
+
+}
