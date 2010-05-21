07ef945b4a86f9b30047ef3c83b9e1c23e124925
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-21 09:48:15 +0000

    Bug fixes for the Imports relation jobs.

--- trunk/src/pt/um/mrc/jobs/imprt/ImportsByPackage.java
+++ trunk/src/pt/um/mrc/jobs/imprt/ImportsByPackage.java
@@ -22,68 +22,72 @@ import pt.um.mrc.util.io.JavaFileInputFormat;
  * @author Tiago Alves Veloso
  */
 
-public class ImportsByPackage {
+public class ImportsByPackage
+{
 
-	static Configuration conf = new Configuration();
-	static FileSystem fs;
-	static Path tmp = new Path("tmp/");
+    static Configuration conf = new Configuration();
+    static FileSystem fs;
+    static Path tmp = new Path("tmp/");
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
 
-		// Initialize FS
-		fs = FileSystem.get(conf);
+        // Initialize FS
+        fs = FileSystem.get(conf);
 
-		// Check Args
-		CheckedJobInfo cji = new CheckedJobInfo(conf, "Usage: ImportsByPackage <in> <our>");
-		String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
+        // Check Args
+        CheckedJobInfo cji = new CheckedJobInfo(conf, "Usage: ImportsByPackage <in> <our>");
+        String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
 
-		runJob1(otherArgs);
+        runJob1(otherArgs);
 
-		// Set up the Cache
-		for (FileStatus fstatus : fs.listStatus(tmp)) {
-			if (!fstatus.isDir()) {
-				DistributedCache.addCacheFile(fstatus.getPath().toUri(), conf);
-			}
-		}
+        // Set up the Cache
+        for (FileStatus fstatus : fs.listStatus(tmp))
+        {
+            if (!fstatus.isDir())
+            {
+                DistributedCache.addCacheFile(fstatus.getPath().toUri(), conf);
+            }
+        }
 
-		runJob2(otherArgs);
+        runJob2(otherArgs);
 
-	}
+    }
 
-	private static void runJob1(String[] otherArgs) throws Exception {
-		Job job1 = new Job(conf, "find project's internal packages");
+    private static void runJob1(String[] otherArgs) throws Exception
+    {
+        Job job1 = new Job(conf, "find project's internal packages");
 
-		JobConfigurer jc1 =
-			new JobConfigurer(ImportsByPackage.class, JavaFileInputFormat.class, new Path(
-				otherArgs[0]), new Path(otherArgs[1]));
+        JobConfigurer jc1 = new JobConfigurer(ImportsByPackage.class, JavaFileInputFormat.class,
+                new Path(otherArgs[0]), tmp);
 
-		MapperConfigurer mc1 =
-			new MapperConfigurer(FindPackagesMapper.class, Text.class, Text.class);
+        MapperConfigurer mc1 = new MapperConfigurer(FindPackagesMapper.class, Text.class,
+                Text.class);
 
-		HadoopJobControl.configureSimpleJob(job1, jc1, mc1, FindPackagesReducer.class);
+        HadoopJobControl.configureSimpleJob(job1, jc1, mc1, FindPackagesReducer.class);
 
-		boolean statusJob1 = job1.waitForCompletion(true);
+        boolean statusJob1 = job1.waitForCompletion(true);
 
-		if (!statusJob1)
-			System.exit(1);
-	}
+        if (!statusJob1)
+            System.exit(1);
+    }
 
-	private static void runJob2(String[] otherArgs) throws Exception {
-		Job job2 = new Job(conf, "find packages imported by a file");
+    private static void runJob2(String[] otherArgs) throws Exception
+    {
+        Job job2 = new Job(conf, "find packages imported by a file");
 
-		JobConfigurer jc2 =
-			new JobConfigurer(ImportsByPackage.class, JavaFileInputFormat.class, new Path(
-				otherArgs[0]), new Path(otherArgs[1]));
+        JobConfigurer jc2 = new JobConfigurer(ImportsByPackage.class, JavaFileInputFormat.class,
+                new Path(otherArgs[0]), new Path(otherArgs[1]));
 
-		MapperConfigurer mc2 =
-			new MapperConfigurer(ImportsByPackageMapper.class, Text.class, Text.class);
+        MapperConfigurer mc2 = new MapperConfigurer(ImportsByPackageMapper.class, Text.class,
+                Text.class);
 
-		HadoopJobControl.configureSimpleJob(job2, jc2, mc2, ImportsByPackageReducer.class);
+        HadoopJobControl.configureSimpleJob(job2, jc2, mc2, ImportsByPackageReducer.class);
 
-		boolean statusJob2 = job2.waitForCompletion(true);
+        boolean statusJob2 = job2.waitForCompletion(true);
 
-		fs.delete(tmp, true);
+        fs.delete(tmp, true);
 
-		System.exit(statusJob2 ? 0 : 1);
-	}
+        System.exit(statusJob2 ? 0 : 1);
+    }
 }
--- trunk/src/pt/um/mrc/jobs/imprt/ImportsCommonMapper.java
+++ trunk/src/pt/um/mrc/jobs/imprt/ImportsCommonMapper.java
@@ -10,16 +10,12 @@ import pt.um.mrc.util.mappers.PackageInfoMapper;
 
 public class ImportsCommonMapper<KI, VI, KO, VO> extends PackageInfoMapper<Text, Text, Text, Text>
 {
-    private Text filename = new Text();
     private Text importedPackage = new Text();
 
     @Override
     protected void map(Text key, Text value, Context context) throws IOException,
             InterruptedException
-    {
-        // Set the key to the filename
-        filename.set(key);
-        
+    {        
         // Find the imported packages
         List<String> importedPackages = ImprtHelper.findImportedPackages(value.toString());
 
@@ -31,7 +27,7 @@ public class ImportsCommonMapper<KI, VI, KO, VO> extends PackageInfoMapper<Text,
                 if (imprtPckg.indexOf(intrnPckg) >= 0)
                 {
                     importedPackage.set(imprtPckg);
-                    context.write(filename, importedPackage);    
+                    context.write(key, importedPackage);    
                 }  
             }
         }
