704b1f7c07444480702e2ea917d653759d7d60cf
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-21 09:51:09 +0000

    Minor tweaks to the job control helper classes.

--- src/pt/um/mrc/util/control/HadoopJobControl.java
+++ src/pt/um/mrc/util/control/HadoopJobControl.java
@@ -1,6 +1,5 @@
 package pt.um.mrc.util.control;
 
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -8,48 +7,51 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class HadoopJobControl {
+public class HadoopJobControl
+{
+    public static void configureSimpleJob(Job job, JobConfigurer jc, MapperConfigurer mc,
+            Class<? extends Reducer<?, ?, ?, ?>> reducer) throws Exception
+    {
 
-	public static void configureSimpleJob(Job job, JobConfigurer jc,
-		MapperConfigurer mc, Class<? extends Reducer<?, ?, ?, ?>> reducer)
-		throws Exception {
+        FileSystem fs = FileSystem.get(job.getConfiguration());
 
-		FileSystem fs = FileSystem.get(job.getConfiguration());
-		
-		// Configure generic Job stuff
-		job.setJarByClass(jc.getClassJar());
-		job.setInputFormatClass(jc.getIntputFormat());
-		
-		FileOutputFormat.setOutputPath(job, jc.getOutputPath());
-		
-		// TODO NOT CHECKED YET!
-		for (FileStatus fstatus : fs.listStatus(jc.getInputPath())) {
-			if (fstatus.isDir()) {
-				FileInputFormat.addInputPath(job, fstatus.getPath());
-			}
-		}
-		
-		// Configure the Mapper Stuff
-		job.setMapperClass(mc.getMapperClass());
-		job.setMapOutputKeyClass(mc.getMapOutKey());
-		job.setMapOutputValueClass(mc.getMapOutValue());
+        // Configure generic Job stuff
+        job.setJarByClass(jc.getClassJar());
+        job.setInputFormatClass(jc.getIntputFormat());
 
-		// Configure the Reducer Class
-		job.setReducerClass(reducer);
+        FileInputFormat.setInputPaths(job, jc.getInputPath());
+        FileOutputFormat.setOutputPath(job, jc.getOutputPath());
 
-	}
-	
-	public static String[] checkArguments(String[] args, CheckedJobInfo cji)
-	{
-		 String[] otherArgs = new GenericOptionsParser(cji.getConf(), args).getRemainingArgs();
-	    
-	    if (otherArgs.length != 2)
+        // TODO NOT CHECKED YET!
+//        for (FileStatus fstatus : fs.listStatus(jc.getInputPath()))
+//        {
+//            if (fstatus.isDir())
+//            {
+//                FileInputFormat.addInputPath(job, fstatus.getPath());
+//            }
+//        }
+
+        // Configure the Mapper Stuff
+        job.setMapperClass(mc.getMapperClass());
+        job.setMapOutputKeyClass(mc.getMapOutKey());
+        job.setMapOutputValueClass(mc.getMapOutValue());
+
+        // Configure the Reducer Class
+        job.setReducerClass(reducer);
+
+    }
+
+    public static String[] checkArguments(String[] args, CheckedJobInfo cji)
+    {
+        String[] otherArgs = new GenericOptionsParser(cji.getConf(), args).getRemainingArgs();
+
+        if (otherArgs.length != 2)
         {
-        	//TODO replace with log4j
+            // TODO replace with log4j
             System.err.println(cji.getUsageMessage());
             System.exit(2);
         }
-	    
-	    return otherArgs;
-	}
+
+        return otherArgs;
+    }
 }
