b5cdc86dfa77eb0be00bb2d06a3af2c62269aefe
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-22 14:11:24 +0000

    Volume by Class, by File and by Package jobs.

--- src/pt/um/mrc/jobs/volume/VolumeByClass.java
+++ src/pt/um/mrc/jobs/volume/VolumeByClass.java
@@ -6,8 +6,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 
+import pt.um.mrc.util.control.CheckedJobInfo;
 import pt.um.mrc.util.control.HadoopJobControl;
 import pt.um.mrc.util.control.JobConfigurer;
 import pt.um.mrc.util.control.MapperConfigurer;
@@ -20,30 +20,27 @@ import pt.um.mrc.util.control.MapperConfigurer;
  * @author Tiago Alves Veloso
  */
 
-public class VolumeByClass {
+public class VolumeByClass
+{
+    public static void main(String[] args) throws Exception
+    {
+        Configuration conf = new Configuration();
+            
+        CheckedJobInfo cji = new CheckedJobInfo(conf, "Usage: VolumeByClass <in> <out>");
+        String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
 
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2) {
-			System.err.println("Usage: VolumeByClass <in> <out>");
-			System.exit(2);
-		}
+        // // Create a new Job
+        Job job = new Job(conf, "compute the LoC volume for each class");
 
-		// // Create a new Job
-		Job job = new Job(conf, "compute the LoC volume for each class");
+        JobConfigurer jc = new JobConfigurer(VolumeByClass.class, TextInputFormat.class, new Path(
+                otherArgs[0]), new Path(otherArgs[1]));
 
-		JobConfigurer jc =
-			new JobConfigurer(VolumeByClass.class, TextInputFormat.class, new Path(otherArgs[0]),
-				new Path(otherArgs[1]));
+        MapperConfigurer mc = new MapperConfigurer(VolumeByClassMapper.class, Text.class,
+                IntWritable.class);
 
-		MapperConfigurer mc =
-			new MapperConfigurer(VolumeByClassMapper.class, Text.class, IntWritable.class);
+        HadoopJobControl.configureSimpleJob(job, jc, mc, VolumeByClassReducer.class);
 
-		HadoopJobControl.configureSimpleJob(job, jc, mc, VolumeByClassReducer.class);
-
-		// Close the Job
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
-
+        // Close the Job
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
--- src/pt/um/mrc/jobs/volume/VolumeByFile.java
+++ src/pt/um/mrc/jobs/volume/VolumeByFile.java
@@ -1,5 +1,17 @@
 package pt.um.mrc.jobs.volume;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+import pt.um.mrc.util.control.CheckedJobInfo;
+import pt.um.mrc.util.control.HadoopJobControl;
+import pt.um.mrc.util.control.JobConfigurer;
+import pt.um.mrc.util.control.MapperConfigurer;
+
 /**
  * This class contains the configuration for the job that relates files with
  * their lines of code volume.
@@ -10,8 +22,25 @@ package pt.um.mrc.jobs.volume;
 
 public class VolumeByFile
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
+        Configuration conf = new Configuration();
+        
+        CheckedJobInfo cji = new CheckedJobInfo(conf, "Usage: VolumeByFile <in> <out>");
+        String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
+        
+        // // Create a new Job
+        Job job = new Job(conf, "compute the LoC volume for each file");
 
+        JobConfigurer jc = new JobConfigurer(VolumeByFile.class, TextInputFormat.class, new Path(
+                otherArgs[0]), new Path(otherArgs[1]));
+
+        MapperConfigurer mc = new MapperConfigurer(VolumeByFileMapper.class, Text.class,
+                IntWritable.class);
+
+        HadoopJobControl.configureSimpleJob(job, jc, mc, VolumeByFileReducer.class);
+
+        // Close the Job
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
