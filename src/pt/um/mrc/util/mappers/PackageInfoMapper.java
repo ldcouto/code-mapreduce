775a9e6d976a0439fca4541509d303ac7825a265
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-22 14:09:02 +0000

    Minot tweaks to the PackageInfoMapper.

--- src/pt/um/mrc/util/mappers/PackageInfoMapper.java
+++ src/pt/um/mrc/util/mappers/PackageInfoMapper.java
@@ -9,43 +9,42 @@ import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Mapper;
 
+public class PackageInfoMapper<KI, VI, KO, VO> extends Mapper<KI, VI, KO, VO>
+{
 
-public class PackageInfoMapper<KI, VI, KO, VO> extends Mapper<KI, VI, KO, VO>{
+    protected ArrayList<String> internalPackages = new ArrayList<String>();
 
-	protected ArrayList<String> internalPackages = new ArrayList<String>();
-    
-	public ArrayList<String> getInternalPackages() {
-		return internalPackages;
-	}
+    public ArrayList<String> getInternalPackages()
+    {
+        return internalPackages;
+    }
 
-	public void setInternalPackages(ArrayList<String> internalPackages) {
-		this.internalPackages = internalPackages;
-	}
+    public void setInternalPackages(ArrayList<String> internalPackages)
+    {
+        this.internalPackages = internalPackages;
+    }
 
-
-
-	@Override
+    @Override
     protected void setup(Context context) throws IOException, InterruptedException
     {
         Path[] localFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
-        
-        //TODO Should this guard stay?
+
+        // TODO Should this guard stay?
         if (localFiles != null)
         {
-	        	
-	        FileReader fr = new FileReader(localFiles[0].toString());
-	        BufferedReader br = new BufferedReader(fr);
-	
-	        String aux;
-	
-	        while ((aux = br.readLine()) != null)
-	        {
-	            internalPackages.add(aux.trim());
-	        }
-	
-	        br.close();
+            FileReader fr = new FileReader(localFiles[0].toString());
+            BufferedReader br = new BufferedReader(fr);
+
+            String aux;
+
+            while ((aux = br.readLine()) != null)
+            {
+                internalPackages.add(aux.trim());
+            }
+
+            br.close();
         }
         super.setup(context);
     }
-	
+
 }
