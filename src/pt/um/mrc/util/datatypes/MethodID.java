93006548ac9afa1858a0a8d3aff6928f87eb8968
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-23 00:20:30 +0000

    Further testing on MethodID (now 100% tested).

--- src/pt/um/mrc/util/datatypes/MethodID.java
+++ src/pt/um/mrc/util/datatypes/MethodID.java
@@ -6,78 +6,74 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.WritableComparable;
 
-
 public class MethodID implements WritableComparable<MethodID> {
-	
+
 	String methodName;
 	String className;
 	String fileName;
 	String packageName;
-	
-	public MethodID(){
-		methodName="";
-		className="";
-		fileName="";
-		packageName="";
+
+	public MethodID() {
+		methodName = "";
+		className = "";
+		fileName = "";
+		packageName = "";
 	}
-	
-	public MethodID(String methodName, String className, String fileName,
-		String packageName) {
+
+	public MethodID(String methodName, String className, String fileName, String packageName) {
 		this.methodName = methodName;
 		this.className = className;
 		this.fileName = fileName;
 		this.packageName = packageName;
 	}
-	
-	public MethodID(String aggre){
+
+	public MethodID(String aggre) {
 		String[] aux = aggre.split("-");
-		packageName=aux[0];
-		fileName=aux[1];
-		className=aux[2];
-		methodName=aux[3];
+		packageName = aux[0];
+		fileName = aux[1];
+		className = aux[2];
+		methodName = aux[3];
 	}
 
 	public String getMethodName() {
 		return methodName;
 	}
-	
+
 	public void setMethodName(String methodName) {
 		this.methodName = methodName;
 	}
-	
+
 	public String getClassName() {
 		return className;
 	}
-	
+
 	public void setClassName(String className) {
 		this.className = className;
 	}
-	
+
 	public String getFileName() {
 		return fileName;
 	}
-	
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	
+
 	public String getPackageName() {
 		return packageName;
 	}
-	
+
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
 	}
 
-	
 	public String prettyString() {
-		return "MethodID[" + "packageName=" + packageName + "fileName=" + fileName + "" +
-				"className=" + className + ", methodName=" + methodName + "]";
+		return "MethodID[" + "packageName=" + packageName + ", fileName=" + fileName
+			+ ", className=" + className + ", methodName=" + methodName + "]";
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(packageName);
 		sb.append('-');
@@ -91,33 +87,28 @@ public class MethodID implements WritableComparable<MethodID> {
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
-		className=in.readUTF();
-		fileName=in.readUTF();
-		methodName=in.readUTF();
-		packageName=in.readUTF();
+		packageName = in.readUTF();
+		fileName = in.readUTF();
+		className = in.readUTF();
+		methodName = in.readUTF();
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-		out.writeUTF(className);
-		out.writeUTF(fileName);
-		out.writeUTF(methodName);
 		out.writeUTF(packageName);
+		out.writeUTF(fileName);
+		out.writeUTF(className);
+		out.writeUTF(methodName);
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result =
-			prime * result + ((className == null) ? 0 : className.hashCode());
-		result =
-			prime * result + ((fileName == null) ? 0 : fileName.hashCode());
-		result =
-			prime * result + ((methodName == null) ? 0 : methodName.hashCode());
-		result =
-			prime * result
-				+ ((packageName == null) ? 0 : packageName.hashCode());
+		result = prime * result + ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((fileName == null) ? 0 : fileName.hashCode());
+		result = prime * result + ((methodName == null) ? 0 : methodName.hashCode());
+		result = prime * result + ((packageName == null) ? 0 : packageName.hashCode());
 		return result;
 	}
 
@@ -156,25 +147,22 @@ public class MethodID implements WritableComparable<MethodID> {
 	@Override
 	public int compareTo(MethodID o) {
 		int cmpPkg = this.packageName.compareTo(o.getPackageName());
-		if (cmpPkg !=0)
+		if (cmpPkg != 0)
 			return cmpPkg;
-		
+
 		int cmpFile = this.fileName.compareTo(o.getFileName());
-		if (cmpFile !=0)
+		if (cmpFile != 0)
 			return cmpFile;
-		
+
 		int cmpClass = this.className.compareTo(o.getClassName());
-		if (cmpClass !=0)
+		if (cmpClass != 0)
 			return cmpClass;
-		
+
 		int cmpMetd = this.methodName.compareTo(o.getMethodName());
-		if (cmpMetd !=0)
+		if (cmpMetd != 0)
 			return cmpMetd;
-		
+
 		return 0;
 	}
 
-
-	
-	
 }
