f024c65d68657da1aba0546c4afe0a1a2108de45
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-24 14:14:10 +0000

    Fix on the identity reducer.

--- trunk/src/pt/um/mrc/util/reducers/IdentityReducer.java
+++ trunk/src/pt/um/mrc/util/reducers/IdentityReducer.java
@@ -1,16 +1,6 @@
 package pt.um.mrc.util.reducers;
 
-import java.io.IOException;
-
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class IdentityReducer<KI, VI, KO, VO> extends Reducer<KI, VI, KO, VO>
-{
-	//TODO This might need to do something
-//    @Override
-//    protected void reduce(KI key, Iterable<VI> values, Context context) throws IOException,
-//            InterruptedException
-//    {
-//        super.reduce(key, values, context);
-//    }
-}
+{}
