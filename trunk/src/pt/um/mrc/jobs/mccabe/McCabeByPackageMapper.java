3454eeb1b7c895e5d4417eb1b1650fa92126ee96
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-24 09:56:51 +0000

    Slight change to LineValues to further differentiate between the operation types.

--- trunk/src/pt/um/mrc/jobs/mccabe/McCabeByPackageMapper.java
+++ trunk/src/pt/um/mrc/jobs/mccabe/McCabeByPackageMapper.java
@@ -1,5 +1,7 @@
 package pt.um.mrc.jobs.mccabe;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,5 +16,11 @@ import pt.um.mrc.util.mappers.LineValuesMapper;
  * @author Tiago Alves Veloso
  */
 
-public class McCabeByPackageMapper extends LineValuesMapper<LongWritable, Text, Text, IntWritable>
-{}
+public class McCabeByPackageMapper extends LineValuesMapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+		lineContents = LineType.PACKAGE;
+		super.setup(context);
+	}
+}
