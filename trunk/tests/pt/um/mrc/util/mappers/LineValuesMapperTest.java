41d8ada380ff819d33d37a62a71cad614ee89e46
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-24 14:16:03 +0000

    Fix on the LineValuesMapper Test.

--- trunk/tests/pt/um/mrc/util/mappers/LineValuesMapperTest.java
+++ trunk/tests/pt/um/mrc/util/mappers/LineValuesMapperTest.java
@@ -3,45 +3,46 @@ package pt.um.mrc.util.mappers;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LineValuesMapperTest {
+import pt.um.mrc.util.mappers.LineValuesMapper.LineType;
 
-    private Mapper<LongWritable, Text, Text, IntWritable> mapper;
+public class LineValuesMapperTest
+{
+
+    private LineValuesMapper<LongWritable, Text, Text, IntWritable> mapper;
     private MapDriver<LongWritable, Text, Text, IntWritable> driver;
 
-	
-	@Before
-	public void setUp() throws Exception {
-		mapper = new LineValuesMapper<LongWritable,Text,Text,IntWritable>();
-		driver = new MapDriver<LongWritable, Text, Text, IntWritable>(mapper);
-	}
+    @Before
+    public void setUp() throws Exception
+    {
+        mapper = new LineValuesMapper<LongWritable, Text, Text, IntWritable>();
+        driver = new MapDriver<LongWritable, Text, Text, IntWritable>(mapper);
+        mapper.lineContents = LineType.CLASS;
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception
+    {}
 
-	@Test
-	public void testMapLongWritableTextContext() {
-		LongWritable inKey = new LongWritable(10);
+    @Test
+    public void testMapLongWritableTextContext()
+    {
+        LongWritable inKey = new LongWritable(10);
 
-		Text inValues =
-			new Text(
-				"pt.um.mrc.jobs.imprt-PkgAndClassMapper.java-PkgAndClassMapper-map[Text key, Text value, Context context]\t5");
+        Text inValues = new Text(
+                "pt.um.mrc.jobs.imprt-PkgAndClassMapper.java-PkgAndClassMapper-map[Text key, Text value, Context context]\t5");
 
-		Text outKey = new Text("pt.um.mrc.jobs.imprt-PkgAndClassMapper.java-PkgAndClassMapper");
+        Text outKey = new Text("pt.um.mrc.jobs.imprt-PkgAndClassMapper.java-PkgAndClassMapper");
 
-		IntWritable outValue = new IntWritable(5);
+        IntWritable outValue = new IntWritable(5);
 
-		driver.withInput(inKey, inValues);
-		driver.withOutput(outKey, outValue);
-		driver.runTest();
-	}
-	
-	
+        driver.withInput(inKey, inValues);
+        driver.withOutput(outKey, outValue);
+        driver.runTest();
+    }
 
 }
