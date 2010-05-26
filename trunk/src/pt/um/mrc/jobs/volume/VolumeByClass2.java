82168acba7d67ebc856d9f9c105f11ea7cab2ca7
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-26 23:02:17 +0000

    Bug fixing the Volume By Class Job.

--- trunk/src/pt/um/mrc/jobs/volume/VolumeByClass2.java
+++ trunk/src/pt/um/mrc/jobs/volume/VolumeByClass2.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import pt.um.mrc.util.control.JobInformable;
+import pt.um.mrc.util.control.JobRunner;
 import pt.um.mrc.util.io.JClassFileInputFormat;
 
 public class VolumeByClass2 implements JobInformable
@@ -39,13 +40,20 @@ public class VolumeByClass2 implements JobInformable
     @Override
     public Class<? extends Reducer<?, ?, ?, ?>> getReducerClass()
     {
-        return VolumeByFileReducer.class;
+        return VolumeByClassReducer2.class;
     }
 
     @Override
     public String getUsage()
     {
         return "Usage: VolumeByClass2 <in> <out>";
+    }
+    
+    public static void main(String[] args)
+    {
+        VolumeByClass2 me = new VolumeByClass2();
+        JobRunner.setJob(args, me);
+        JobRunner.runJob();
     }
 
 }
