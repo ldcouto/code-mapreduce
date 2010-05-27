4c268e3df1a23a470036e33d141302e8c09c04d1
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-27 14:04:54 +0000

    Refactoring the Volume By Class Job.

--- trunk/src/pt/um/mrc/jobs/volume/VolumeByClassReducer2.java
+++
@@ -1,9 +0,0 @@
-package pt.um.mrc.jobs.volume;
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-
-import pt.um.mrc.util.reducers.SumReducer;
-
-public class VolumeByClassReducer2 extends SumReducer<Text, IntWritable, Text, IntWritable>
-{}
