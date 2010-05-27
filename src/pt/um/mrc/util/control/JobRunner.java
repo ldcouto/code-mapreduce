dbfbb8d9d43fd0fd3b466547837e8993e57fa9bb
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-27 23:01:27 +0000

    JobRunner support for Distributed Cache (accidentally left off the previous commit).

--- src/pt/um/mrc/util/control/JobRunner.java
+++ src/pt/um/mrc/util/control/JobRunner.java
@@ -3,6 +3,10 @@ package pt.um.mrc.util.control;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 
 /**
@@ -11,88 +15,83 @@ import org.apache.hadoop.mapreduce.Job;
  * @author Tiago Alves Veloso
  * @author Luis Duarte Couto
  */
-public class JobRunner
-{
-    protected static Log LOG = LogFactory.getLog(JobRunner.class);
-    
-    /** The configuration. */
-    protected static Configuration conf;
+public class JobRunner {
 
-    /** The auxiliary CheckedJobInfo. */
-    protected static CheckedJobInfo cji;
+	protected static Log LOG = LogFactory.getLog(JobRunner.class);
 
-    /** The auxiliary JobConfigurer. */
-    protected static JobConfigurer jc;
+	/** The configuration. */
+	protected static Configuration conf;
 
-    /** The auxiliary MapperConfigurer. */
-    protected static MapperConfigurer mc;
+	/** The auxiliary CheckedJobInfo. */
+	protected static CheckedJobInfo cji;
 
-    /** The Job to configure. */
-    protected static Job job;
+	/** The auxiliary JobConfigurer. */
+	protected static JobConfigurer jc;
 
-    
-    
-    
+	/** The auxiliary MapperConfigurer. */
+	protected static MapperConfigurer mc;
+
+	/** The Job to configure. */
+	protected static Job job;
+
 	public static Configuration getConf() {
 		return conf;
 	}
 
 	/**
-     * Sets up a job.
-     * 
-     * @param args
-     *            the arguments from the command line
-     * @param ji
-     *            the JobInformaer for the job to configure
-     * @throws Exception 
-     * @throws Exception
-     *             the exception
-     */
-    public static void setJob(String[] args, JobInformable ji)
-    {
-        conf = new Configuration();
-        cji = new CheckedJobInfo(ji.getUsage(), conf, ji.getArgCount());
+	 * Sets up a job.
+	 * 
+	 * @param args
+	 *            the arguments from the command line
+	 * @param ji
+	 *            the JobInformaer for the job to configure
+	 * @throws Exception
+	 * @throws Exception
+	 *             the exception
+	 */
+	public static void setJob(String[] args, JobInformable ji) {
+		conf = new Configuration();
+		cji = new CheckedJobInfo(ji.getUsage(), conf, ji.getArgCount());
 
-        String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
+		String[] otherArgs = HadoopJobControl.checkArguments(args, cji);
 
-        mc = new MapperConfigurer(ji.getMapperClass(), ji.getMapperKeyClass(), ji
-                .getMapperValueClass());
+		mc =
+				new MapperConfigurer(ji.getMapperClass(), ji.getMapperKeyClass(), ji
+						.getMapperValueClass());
 
-        jc = new JobConfigurer(ji.getClass(),ji.getInputFormatClass(), otherArgs);
+		jc = new JobConfigurer(ji.getClass(), ji.getInputFormatClass(), otherArgs);
 
-        try
-        {
-            job = new Job(conf);
-            HadoopJobControl.configureSimpleJob(job, jc, mc, ji.getReducerClass());
+		try {
+			job = new Job(conf);
+			HadoopJobControl.configureSimpleJob(job, jc, mc, ji.getReducerClass());
 
-        } catch (Exception e)
-        {
-            LOG.fatal(e.getMessage());
-            System.exit(2);
-        }
-    }
+		} catch (Exception e) {
+			LOG.fatal(e.getMessage());
+			System.exit(2);
+		}
+	}
 
-    /**
-     * Run job.
-     * 
-     * @throws Exception
-     *             the exception
-     */
-    public static int runJob()
-    {
-        int r = 2;
-        
-        try{
-        r = job.waitForCompletion(true) ? 0 : 1;
-        } catch (Exception e) {
-            LOG.fatal(e.getMessage());
-            System.exit(2);
-        }
-        
-        return r;
-    }
-    
-    public static int runDoubleJob(JobInformable job1, JobInformable job2, String tempFolder, String[] args){
+	/**
+	 * Run job.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	public static int runJob() {
+		int r = 2;
+
+		try {
+			r = job.waitForCompletion(true) ? 0 : 1;
+		} catch (Exception e) {
+			LOG.fatal(e.getMessage());
+			System.exit(2);
+		}
+
+		return r;
+	}
+
+	public static int runDoubleJob(JobInformable job1, JobInformable job2, String tempFolder,
+			String[] args) {
 		String[] argsJob1 = { args[1], tempFolder };
 		JobRunner.setJob(argsJob1, job1);
 
@@ -104,8 +103,16 @@ public class JobRunner
 		JobRunner.setJob(argsJob2, job2);
 
 		status = JobRunner.runJob();
-		
+
 		return status;
-    }
+	}
+
+	public static void configureDistCache(Path path) throws Exception {
+		for (FileStatus fstatus : FileSystem.get(conf).listStatus(path)) {
+			if (!fstatus.isDir()) {
+				DistributedCache.addCacheFile(fstatus.getPath().toUri(), conf);
+			}
+		}
+	}
 
 }
