1cb42feb887b8940d66bad23cc28f5ec117fb5d1
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-28 22:05:41 +0000

    Reverted a change in the Cache Job.

--- trunk/src/pt/um/mrc/util/mappers/CachedPackageInfoMapper.java
+++ trunk/src/pt/um/mrc/util/mappers/CachedPackageInfoMapper.java
@@ -8,56 +8,55 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class CachedPackageInfoMapper<KI, VI, KO, VO> extends Mapper<KI, VI, KO, VO>
-{
-    static final Log LOG = LogFactory.getLog(CachedPackageInfoMapper.class);
+public class CachedPackageInfoMapper<KI, VI, KO, VO> extends Mapper<KI, VI, KO, VO> {
 
-    protected Map<String, ArrayList<String>> internalClassPkgInfo = new HashMap<String, ArrayList<String>>();
+	protected Map<String, ArrayList<String>> internalClassPkgInfo =
+			new HashMap<String, ArrayList<String>>();
 
-    public Map<String, ArrayList<String>> getInternalPackages()
-    {
-        return internalClassPkgInfo;
-    }
+	public Map<String, ArrayList<String>> getInternalPackages() {
+		return internalClassPkgInfo;
+	}
 
-    public void setInternalPackages(Map<String, ArrayList<String>> internalPackages)
-    {
-        this.internalClassPkgInfo = internalPackages;
-    }
+	public void setInternalPackages(Map<String, ArrayList<String>> internalPackages) {
+		this.internalClassPkgInfo = internalPackages;
+	}
 
-    @Override
-    protected void setup(Context context) throws IOException, InterruptedException
-    {
-        Path[] localFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+		Path[] localFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
 
-        buildCache(localFiles);
+		buildCache(localFiles);
 
-        super.setup(context);
-    }
+		super.setup(context);
+	}
 
-    public void buildCache(Path[] localFiles) throws IOException
-    {
-        FileReader fr = new FileReader(localFiles[0].toString());
-        BufferedReader br = new BufferedReader(fr);
+	public void buildCache(Path[] localFiles) throws IOException {
 
-        String line;
-        String[] pkgAndClass;
-        ArrayList<String> classes;
+		// TODO Should this guard stay?
+		if (localFiles != null) {
+			FileReader fr = new FileReader(localFiles[0].toString());
+			BufferedReader br = new BufferedReader(fr);
 
-        while ((line = br.readLine()) != null)
-        {
-            pkgAndClass = line.split("\\t");
+			String line;
+			String[] pkgAndClass;
+			ArrayList<String> classes;
 
-            classes = new ArrayList<String>(Arrays.asList(pkgAndClass[1].replaceAll("\\{|\\}", "")
-                    .trim().split(" ")));
-            internalClassPkgInfo.put(pkgAndClass[0], classes);
-        }
-        br.close();
-    }
+			while ((line = br.readLine()) != null) {
+				pkgAndClass = line.split("\\t");
+
+				System.out.println(pkgAndClass[0]);
+
+				classes =
+						new ArrayList<String>(Arrays.asList(pkgAndClass[1]
+								.replaceAll("\\{|\\}", "").trim().split(" ")));
+				internalClassPkgInfo.put(pkgAndClass[0], classes);
+			}
+			br.close();
+		}
+	}
 
 }
