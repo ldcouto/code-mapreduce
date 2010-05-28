86de3caf31400d1e86b8937566fd0911705a34b6
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-28 14:52:41 +0000

    Minor tweaks.

--- src/pt/um/mrc/jobs/mccabe/McCabeMiscMapper.java
+++ src/pt/um/mrc/jobs/mccabe/McCabeMiscMapper.java
@@ -4,25 +4,23 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import pt.um.mrc.lib.McCabeHelper;
-import pt.um.mrc.util.datatypes.ClassID;
 
-public class McCabeMiscMapper extends Mapper<ClassID, Text, Text, IntWritable>
+public class McCabeMiscMapper extends Mapper<WritableComparable<?>, Text, Text, IntWritable>
 {
     private Text keyOut = new Text();
     private IntWritable mcCabeNumber = new IntWritable();
     
     @Override
-    protected void map(ClassID key, Text value, Context context)
+    protected void map(WritableComparable<?> key, Text value, Context context)
             throws IOException, InterruptedException
     {
-    //	System.out.println("Vou processar "+ key.toString() + " " + value.toString());
         keyOut.set(key.toString());
         mcCabeNumber.set(McCabeHelper.countMcCabe(value.toString()));
         
         context.write(keyOut, mcCabeNumber);
     }
-    
 }
