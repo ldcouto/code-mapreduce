e98dd45a3dc896597273f698769156dc7ea53470
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-28 10:14:05 +0000

    Updated tests for the Previously changed Import and package Jobs

--- tests/pt/um/mrc/jobs/imprt/PkgAndClassMapperTest.java
+++ tests/pt/um/mrc/jobs/imprt/PkgAndClassMapperTest.java
@@ -6,16 +6,18 @@ import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.um.mrc.util.datatypes.FileID;
+
 public class PkgAndClassMapperTest
 {
-    private Mapper<Text, Text, Text, Text> mapper;
-    private MapDriver<Text, Text, Text, Text> driver;
+    private Mapper<FileID, Text, Text, Text> mapper;
+    private MapDriver<FileID, Text, Text, Text> driver;
 
     @Before
     public void setUp() throws Exception
     {
         mapper = new PkgAndClassMapper();
-        driver = new MapDriver<Text, Text, Text, Text>(mapper);
+        driver = new MapDriver<FileID, Text, Text, Text>(mapper);
     }
 
     @Test
@@ -25,7 +27,7 @@ public class PkgAndClassMapperTest
     	sb.append("package pt.um.mrc.jobs.imprt;\n");
     	sb.append("public class Foo_Bar{}\n");
     	sb.append("public class Foo_Bar2{}");
-        driver.withInput(new Text("File1.java"), new Text(sb.toString()));
+        driver.withInput(new FileID("File1.java", "pt.um.mrc.jobs.imprt"), new Text(sb.toString()));
         
         driver.withOutput(new Text("pt.um.mrc.jobs.imprt"), new Text("pt.um.mrc.jobs.imprt.Foo_Bar"));
         driver.withOutput(new Text("pt.um.mrc.jobs.imprt"), new Text("pt.um.mrc.jobs.imprt.Foo_Bar2"));
--- tests/pt/um/mrc/jobs/pckg/PackageByFileMapperTest.java
+++ tests/pt/um/mrc/jobs/pckg/PackageByFileMapperTest.java
@@ -6,23 +6,25 @@ import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.um.mrc.util.datatypes.FileID;
+
 public class PackageByFileMapperTest
 {
 
-    private Mapper<Text, Text, Text, Text> mapper;
-    private MapDriver<Text, Text, Text, Text> driver;
+    private Mapper<FileID, Text, Text, Text> mapper;
+    private MapDriver<FileID, Text, Text, Text> driver;
     
     @Before
     public void setUp() throws Exception
     {
         mapper = new PackageByFileMapper();
-        driver = new MapDriver<Text, Text, Text, Text>(mapper);
+        driver = new MapDriver<FileID, Text, Text, Text>(mapper);
     }
 
     @Test
     public final void testMap()
     {
-        driver.withInput(new Text("File1.java"), new Text("package pt.um.mrc.jobs.pckg;"));
+        driver.withInput(new FileID("File1.java", "package pt.um.mrc.jobs.pckg;"), new Text("package pt.um.mrc.jobs.pckg;"));
         driver.withOutput(new Text("File1.java"), new Text("pt.um.mrc.jobs.pckg"));
         driver.runTest();
     }
