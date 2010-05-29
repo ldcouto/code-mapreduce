5a834a2ee2aae296ad0effcd9eb7401059853ba2
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-29 10:00:07 +0000

    Minor fix to the ImportsByFileTest.

--- trunk/tests/pt/um/mrc/jobs/imprt/ImportsByFileTest.java
+++ trunk/tests/pt/um/mrc/jobs/imprt/ImportsByFileTest.java
@@ -1,90 +1,90 @@
-//package pt.um.mrc.jobs.imprt;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.apache.hadoop.io.Text;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import pt.um.mrc.util.io.JavaFileInputFormat;
-//
-//public class ImportsByFileTest
-//{
-//    private ImportsByFile driver;
-//
-//    @Before
-//    public void setUp() throws Exception
-//    {
-//        driver = new ImportsByFile();
-//    }
-//
-//    @Test
-//    public final void testGetInputFormatClass()
-//    {
-//        Class<?> expected = JavaFileInputFormat.class;
-//        
-//        //Class<?> actual = driver.g();
-//        
-//        //assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetMapperClass()
-//    {
-//        Class<?> expected = ImportsByFileMapper.class;
-//        
-//        Class<?> actual = driver.getMapperClass();
-//        
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetMapperKeyClass()
-//    {
-//        Class<?> expected = Text.class;
-//        
-//        Class<?> actual = driver.getMapperKeyOutClass();
-//        
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetMapperValueClass()
-//    {
-//        Class<?> expected = Text.class;
-//        
-//        Class<?> actual = driver.getMapperValueOutClass();
-//        
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetReducerClass()
-//    {
-//        Class<?> expected = ImportsByFileReducer.class;
-//        
-//        Class<?> actual = driver.getReducerClass();
-//        
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetUsage()
-//    {
-//        String expected = "Usage: ImportsByFile <cache> <out>";
-//        
-//        String actual = driver.getUsage();
-//        
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public final void testGetArgCount()
-//    {
-//        int expected = 2;
-//        
-//        int actual = driver.getArgCount();
-//        
-//        assertEquals(expected, actual);
-//    }
-//}
+package pt.um.mrc.jobs.imprt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.hadoop.io.Text;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.um.mrc.util.io.JavaFileInputFormat;
+
+public class ImportsByFileTest
+{
+    private ImportsByFile driver;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        driver = new ImportsByFile();
+    }
+
+    @Test
+    public final void testGetInputFormatClass()
+    {
+        Class<?> expected = JavaFileInputFormat.class;
+        
+        Class<?> actual = driver.getInputFormatClass();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetMapperClass()
+    {
+        Class<?> expected = ImportsByFileMapper.class;
+        
+        Class<?> actual = driver.getMapperClass();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetMapperKeyClass()
+    {
+        Class<?> expected = Text.class;
+        
+        Class<?> actual = driver.getMapperKeyOutClass();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetMapperValueClass()
+    {
+        Class<?> expected = Text.class;
+        
+        Class<?> actual = driver.getMapperValueOutClass();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetReducerClass()
+    {
+        Class<?> expected = ImportsByFileReducer.class;
+        
+        Class<?> actual = driver.getReducerClass();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetUsage()
+    {
+        String expected = "Usage: ImportsByFile <cache> <out>";
+        
+        String actual = driver.getUsage();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testGetArgCount()
+    {
+        int expected = 2;
+        
+        int actual = driver.getArgCount();
+        
+        assertEquals(expected, actual);
+    }
+}
