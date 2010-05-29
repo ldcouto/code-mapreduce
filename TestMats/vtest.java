ed556539386b2fddbbd5a2f8b15e4d1310d059a9
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-29 11:16:19 +0000

    Tests for Visitors.

---
+++ TestMats/vtest.java
@@ -0,0 +1,58 @@
+package pt.um.mrc.jobs.imprt;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import pt.um.mrc.lib.PckgHelper;
+
+public interface jnter{
+	public void meth1t();
+	public void meth1();
+}
+
+
+
+public class FindPackagesMapper extends Mapper<Text, Text, Text, Text> {
+
+	public enum Day {
+	    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, 
+	    THURSDAY, FRIDAY, SATURDAY 
+	}
+
+	
+	public FindPackagesMapper(){}
+	
+	private Text packge;
+	
+	int i=0;
+	
+	static {;
+		if (true)
+			;
+	}
+	
+	//here is some commentary
+
+
+	@Override
+	protected void map(Text key, Text value, Context context) throws IOException,
+			InterruptedException {
+		packge.set(PckgHelper.findPackage(value.toString()));
+
+		context.write(packge, new Text(""));
+	}
+
+
+	/**
+	 * Here's Javadoc!
+	 * @param n
+	 * @param arg
+	 */
+	public void visit(MethodDeclaration n, Object arg) {
+		System.out.println(n.toString());
+		System.out.println(n.haha());
+
+	}
+}
---
+++ tests/pt/um/mrc/util/io/GrabClassAndPkgVisitorTest.java
@@ -0,0 +1,58 @@
+package pt.um.mrc.util.io;
+
+import static org.junit.Assert.*;
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.io.Text;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.um.mrc.util.datatypes.ClassID;
+
+
+public class GrabClassAndPkgVisitorTest {
+
+	GrabClassAndPkgVisitor visitor;
+	CompilationUnit cu;
+	String fileName = "TestMats/vtest.java";
+	Map<ClassID, Text> elems;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		FileInputStream in = new FileInputStream(fileName);
+		
+		visitor = new GrabClassAndPkgVisitor();
+		try {
+			// parse the file
+			cu = JavaParser.parse(in);
+		} finally {
+			in.close();
+		}
+		
+		elems = new HashMap<ClassID, Text>();
+		visitor.init(fileName, cu.getPackage().getName().toString(), elems);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVisitClassOrInterfaceDeclarationObject() {
+		visitor.visit(cu, null);
+		
+		HashMap<ClassID,Text> expected = new HashMap<ClassID,Text>();
+		expected.put(new ClassID("FindPackagesMapper", fileName, "pt.um.mrc.jobs.imprt"), new Text(""));
+		
+		assertEquals(expected, elems);
+		
+	}
+
+}
