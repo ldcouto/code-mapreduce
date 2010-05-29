d41a92e62eb79102be7b04535e137fcf4eaf0379
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-29 23:49:48 +0000

    Small fix. MiscVisitor no longer grabs interfaces.

--- trunk/src/pt/um/mrc/util/io/GrabClassMiscVisitor.java
+++ trunk/src/pt/um/mrc/util/io/GrabClassMiscVisitor.java
@@ -12,16 +12,20 @@ import pt.um.mrc.util.datatypes.ClassID;
 public class GrabClassMiscVisitor extends GrabbingVisitor<ClassID> {
 
 	public void visit(ClassOrInterfaceDeclaration c, Object arg) {
+		if (!c.isInterface()){
 		ClassID aux = new ClassID(c.getName(), fileName, packageName);
 		StringBuilder sb = new StringBuilder();
 		
 		for (BodyDeclaration td : c.getMembers()) {
 			if (!(td instanceof MethodDeclaration) && !(td instanceof ConstructorDeclaration)) {
+				td.setAnnotations(null);
+				td.setJavaDoc(null);
 				sb.append(td.toString());
 				sb.append("\n");
 			}
 		}
 
 		elems.put(aux, new Text(sb.toString()));
+		}
 	}
 }
