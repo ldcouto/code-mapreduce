1f68e5857a9b5321c2bfacc7c205ef24f3c016e3
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-29 22:46:33 +0000

    Tests for the PkgAndClass reader / input format.

---
+++ tests/pt/um/mrc/util/io/JClassAndPkgInputFormatTest.java
@@ -0,0 +1,63 @@
+package pt.um.mrc.util.io;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.JobID;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.um.mrc.util.datatypes.ClassID;
+
+
+public class JClassAndPkgInputFormatTest {
+
+	
+	JClassAndPkgInputFormat iformat;
+	
+	@Before
+	public void setUp() throws Exception {
+		iformat = new JClassAndPkgInputFormat();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+    @Test
+    public final void testIsSplitable()
+    {
+        JobContext context = new JobContext(new Configuration(), new JobID());
+        Path filename = new Path("Some File");
+
+        boolean result = iformat.isSplitable(context, filename);
+
+        assertFalse(result);
+    }
+
+
+	@Test
+	public void testCreateRecordReaderInputSplitTaskAttemptContext() throws IOException, InterruptedException     {
+        InputSplit split = null;
+        TaskAttemptContext context = null;
+
+        RecordReader<ClassID, Text> result;
+
+        result = iformat.createRecordReader(split, context);
+
+        assertNotNull(result);
+        assertTrue(result instanceof JClassAndPkgRecordReader);
+	}
+
+}
