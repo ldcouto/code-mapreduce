fe86fc4c68c79f07ef58290e00fee27f99cc3b89
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-05-31 20:37:12 +0000

    Added javadoc.

--- src/pt/um/mrc/util/reducers/IdentityReducer.java
+++ src/pt/um/mrc/util/reducers/IdentityReducer.java
@@ -3,10 +3,7 @@ package pt.um.mrc.util.reducers;
 import org.apache.hadoop.mapreduce.Reducer;
 
 /**
- * The Class IdentityReducer.
- * 
- * This class simply outputs the Key and Value pairs as inputed by a Mapper
- * class. It completely ignores the Grouping stage.
+ * This reducer simply outputs the Key and Value pairs as inputed by a mapper.
  * 
  * @param <KI>
  *            the generic type
