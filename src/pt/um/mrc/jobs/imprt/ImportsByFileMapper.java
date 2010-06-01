ab7861c892cfdab01892e02d20017d00c0e0aa47
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-01 18:08:52 +0000

    Fixed the Imports Job.

--- src/pt/um/mrc/jobs/imprt/ImportsByFileMapper.java
+++ src/pt/um/mrc/jobs/imprt/ImportsByFileMapper.java
@@ -1,6 +1,10 @@
 package pt.um.mrc.jobs.imprt;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.Text;
+
+import pt.um.mrc.lib.PckgHelper;
 
 /**
  * This class is the mapper for the {@link ImportsByFile} job. It extends
@@ -11,4 +15,13 @@ import org.apache.hadoop.io.Text;
  */
 
 public class ImportsByFileMapper extends ImportsCommonMapper<Text, Text, Text, Text>
-{}
+{
+    @Override
+    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException
+    {
+        String filename = key.toString();
+        String packageName = PckgHelper.findPackage(value.toString());
+        Text newKey = new Text(packageName + "." + filename);  
+        super.map(newKey, value, context);
+    }
+}
