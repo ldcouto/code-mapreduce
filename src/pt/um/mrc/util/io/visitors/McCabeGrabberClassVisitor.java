86d3c02ee65216e0d20665fc39fb03f6665c08c9
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-02 15:55:28 +0000

    Small fix and renaming.

---
+++ src/pt/um/mrc/util/io/visitors/McCabeGrabberClassVisitor.java
@@ -0,0 +1,32 @@
+package pt.um.mrc.util.io.visitors;
+
+import japa.parser.ast.body.BodyDeclaration;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.InitializerDeclaration;
+
+import org.apache.hadoop.io.Text;
+
+import pt.um.mrc.util.datatypes.ClassID;
+
+public class McCabeGrabberClassVisitor extends GrabbingVisitor<ClassID> {
+
+	public void visit(ClassOrInterfaceDeclaration c, Object arg) {
+		if (!c.isInterface()) {
+			ClassID aux = new ClassID(c.getName(), fileName, packageName);
+			StringBuilder sb = new StringBuilder();
+
+			for (BodyDeclaration td : c.getMembers()) {
+				if (td instanceof ClassOrInterfaceDeclaration)
+					visit((ClassOrInterfaceDeclaration) td, arg);
+				else if (td instanceof InitializerDeclaration) {
+					td.setAnnotations(null);
+					td.setJavaDoc(null);
+					sb.append(td.toString());
+					sb.append("\n");
+				}
+			}
+			if (sb.toString().length() > 0)
+				elems.put(aux, new Text(sb.toString()));
+		}
+	}
+}
