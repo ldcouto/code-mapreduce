10f2f430249c0d37541d0a527960e0b9b2d8cf90
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-02 16:18:08 +0000

    Deleting old visitors.

--- src/pt/um/mrc/util/io/visitors/GrabbingVisitor.java
+++ src/pt/um/mrc/util/io/visitors/GrabbingVisitor.java
@@ -8,23 +8,51 @@ import java.util.Map;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
-public abstract class GrabbingVisitor<ID extends WritableComparable<ID>> extends VoidVisitorAdapter<Object>
-{
-    String fileName;
-    String packageName;
-    Map<ID, Text> elems = new HashMap<ID, Text>();
+/**
+ * The Class GrabbingVisitor defines a common way to develop Visitors for use with the
+ * {@link JRecordReader}.
+ *	<br><br>
+ *	Grabbing visitors are initialized inside the RecordReader and then ran to construct a 
+ *	map of elements to be passed to the Mapper. These visitors all have access to the same map
+ *	as the RecordReader while also having knowledge of the package and name of the file they are visiting
+ *	so as to properly construct a their keys.
+ *
+ * @param <ID> the generic type of the key.
+ *  
+ * @author Luis Duarte Couto
+ * @author Tiago Alves Veloso 
+ * 
+ */
+public abstract class GrabbingVisitor<ID extends WritableComparable<ID>> extends
+		VoidVisitorAdapter<Object> {
 
-    public GrabbingVisitor()
-    {
-        super();
-    }
+	/** The file name. */
+	String fileName;
+	
+	/** The package name. */
+	String packageName;
+	
+	/** The elements map. */
+	Map<ID, Text> elems = new HashMap<ID, Text>();
 
-    public void init(String fileName, String packageName, Map<ID, Text> elems)
-    {
-        this.fileName = fileName;
-        this.packageName = packageName;
-        this.elems = elems;
-    }
+	/**
+	 * Instantiates a new grabbing visitor.
+	 */
+	public GrabbingVisitor() {
+		super();
+	}
 
+	/**
+	 * Initializess the.
+	 *
+	 * @param fileName the file name
+	 * @param packageName the package name
+	 * @param elems the elems
+	 */
+	public void init(String fileName, String packageName, Map<ID, Text> elems) {
+		this.fileName = fileName;
+		this.packageName = packageName;
+		this.elems = elems;
+	}
 
 }
