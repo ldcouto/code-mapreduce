77027440e4255287e8d1ca433b0236cb0088d108
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-03 19:19:27 +0000

    Added Javadoc.

--- src/pt/um/mrc/jobs/volume2/VolumeByFileReducer.java
+++ src/pt/um/mrc/jobs/volume2/VolumeByFileReducer.java
@@ -3,10 +3,15 @@ package pt.um.mrc.jobs.volume2;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
+import pt.um.mrc.jobs.volume.VolumeByFile;
 import pt.um.mrc.util.reducers.SumReducer;
 
 /**
- * The Class VolumeByFileReducer.
+ * This class is the reducer for the {@link VolumeByFile} job. It exctends the
+ * {@link SumReducer} class which handles the reduce step for this job.
+ * 
+ * @author Luis Duarte Couto
+ * @author Tiago Alves Veloso
  */
 public class VolumeByFileReducer extends SumReducer<Text, IntWritable, Text, IntWritable>
 {}
