408c71e3b8d74aa688ec81eb275b52293b0d60f7
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-03 19:19:54 +0000

    Added Javadoc

--- trunk/src/pt/um/mrc/jobs/volume2/VolumeByFileMapper.java
+++ trunk/src/pt/um/mrc/jobs/volume2/VolumeByFileMapper.java
@@ -6,12 +6,14 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import pt.um.mrc.jobs.volume.VolumeByFile;
 import pt.um.mrc.lib.PckgHelper;
 import pt.um.mrc.lib.VolumeHelper;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class VolumeByFileMapper.
+ * This class is the mapper for the {@link VolumeByFile} job. It extends a standard Hadoop Mapper.
+ * Since it is not fed by a specialized RecordReader, this Mapper must discard the input key and
+ * manually construct a new one.
  */
 public class VolumeByFileMapper extends Mapper<Text, Text, Text, IntWritable> {
 
@@ -37,7 +39,7 @@ public class VolumeByFileMapper extends Mapper<Text, Text, Text, IntWritable> {
 		Text newKey = new Text(packageName + "-" + filename);
 		super.map(newKey, value, context);
 		lines.set(VolumeHelper.countLoCUnformatted(value.toString()));
-		context.write(key, lines);
+		context.write(newKey, lines);
 	}
 
 	/** The number of lines of code in the body of a method. */
