25c6becfa0bf1ab852e58e05ebf77ab4a1e76469
ldcouto <ldcouto@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-06-04 15:26:49 +0000

    Removed useless class.

--- trunk/src/pt/um/mrc/jobs/volume2/VolumeByFile.java
+++ trunk/src/pt/um/mrc/jobs/volume2/VolumeByFile.java
@@ -1,11 +1,14 @@
 package pt.um.mrc.jobs.volume2;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import pt.um.mrc.jobs.NewVolumeJobsCommon;
 import pt.um.mrc.util.control.JobInformable;
 import pt.um.mrc.util.control.JobRunner;
+import pt.um.mrc.util.io.iformats.JavaFileInputFormat;
 
 /**
  * This job relates files with their volume. <br>
@@ -26,50 +29,46 @@ import pt.um.mrc.util.control.JobRunner;
  * @author Luis Duarte Couto
  * @author Tiago Alves Veloso
  */
-public class VolumeByFile extends NewVolumeJobsCommon implements JobInformable
-{
+public class VolumeByFile implements JobInformable {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see pt.um.mrc.util.control.JobInformable#getMapperClass()
-     */
-    public Class<? extends Mapper<?, ?, ?, ?>> getMapperClass()
-    {
-        return VolumeByFileMapper.class;
-    }
+	public Class<? extends Mapper<?, ?, ?, ?>> getMapperClass() {
+		return VolumeByFileMapper.class;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see pt.um.mrc.util.control.JobInformable#getReducerClass()
-     */
-    public Class<? extends Reducer<?, ?, ?, ?>> getReducerClass()
-    {
-        return VolumeByFileReducer.class;
-    }
+	public Class<? extends Reducer<?, ?, ?, ?>> getReducerClass() {
+		return VolumeByFileReducer.class;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see pt.um.mrc.util.control.JobInformable#getUsage()
-     */
-    public String getUsage()
-    {
-        return "Usage: VolumeByFile <in> <out>";
-    }
+	public String getUsage() {
+		return "Usage: VolumeByFile <in> <out>";
+	}
 
-    /**
-     * The main method.
-     * 
-     * @param args
-     *            the arguments from the command line, the input and the output
-     *            folders
-     */
-    public static void main(String[] args)
-    {
-        VolumeByFile me = new VolumeByFile();
-        System.exit(JobRunner.startJob(args, me));
-    }
+	public int getArgCount() {
+		return 2;
+	}
+
+	public Class<? extends InputFormat<?, ?>> getInputFormatClass() {
+		return JavaFileInputFormat.class;
+	}
+
+	public Class<?> getMapperKeyOutClass() {
+		return Text.class;
+	}
+
+	public Class<?> getMapperValueOutClass() {
+		return IntWritable.class;
+	}
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args
+	 *            the arguments from the command line, the input and the output
+	 *            folders
+	 */
+	public static void main(String[] args) {
+		VolumeByFile me = new VolumeByFile();
+		System.exit(JobRunner.startJob(args, me));
+	}
 
 }
