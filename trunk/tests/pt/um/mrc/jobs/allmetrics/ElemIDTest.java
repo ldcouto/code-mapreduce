7b6bcae7fa185566de668816e91be8177a3020b6
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-09-13 12:47:28 +0000

    Some preliminary tests to the new changes.

---
+++ trunk/tests/pt/um/mrc/jobs/allmetrics/ElemIDTest.java
@@ -0,0 +1,118 @@
+package pt.um.mrc.jobs.allmetrics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.um.mrc.util.datatypes.IDType;
+
+public class ElemIDTest {
+
+	@Before
+	public void setUp()
+	{
+		ElemID id = new ElemID("ElemMethod","ElemClass","ElemFile","ElemPackage",IDType.METHOD,"ElemMetric");
+	}
+	
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testElemID_Constructor_NoParams() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testElemID_Constructor_Parameters() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMetricName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMetricName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMethodName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMethodName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClassName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetClassName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFileName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPackageName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPackageName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadFields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/tests/pt/um/mrc/jobs/allmetrics/GrabEverythingVisitorTest.java
@@ -0,0 +1,63 @@
+package pt.um.mrc.jobs.allmetrics;
+
+import static org.junit.Assert.assertEquals;
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.io.Text;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.um.mrc.util.datatypes.IDType;
+
+public class GrabEverythingVisitorTest {
+
+	private GrabEverythingVisitor visitor;
+	private CompilationUnit cu;
+	private String fileName = "TestMats/vtest.java";
+	private Map<ElemID, Text> elems;
+
+	@Before
+	public void setUp() throws Exception {
+		FileInputStream in = new FileInputStream(fileName);
+
+		visitor = new GrabEverythingVisitor();
+		try {
+			// parse the file
+			cu = JavaParser.parse(in);
+		} finally {
+			in.close();
+		}
+
+		elems = new HashMap<ElemID, Text>();
+		visitor.init(fileName, cu.getPackage().getName().toString(), elems);
+	}
+
+	@Test
+	public void testVisit() {
+		visitor.visit(cu, null);
+
+		HashMap<ElemID, Text> expected = new HashMap<ElemID, Text>();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("private Text packge;\n");
+		sb.append("int i = 0;\n");
+		sb.append("static {\n");
+		sb.append("    if (true) i = 0;\n");
+		sb.append("}\n");
+
+		ElemID key = new ElemID("", "FindPackagesMapper", fileName, "pt.um.mrc.jobs.imprt",IDType.CLASS, null);
+		
+		expected.put(key, new Text(sb.toString()));
+			
+		ElemID key2 = new ElemID("", "A", fileName, "pt.um.mrc.jobs.imprt",IDType.CLASS, null);
+		
+		expected.put(key2, new Text(""));
+
+		assertEquals(expected, elems);
+	}
+}
