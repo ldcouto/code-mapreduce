13cf7bc7fda8300e3770582462f27dde5725a446
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-09-14 08:51:32 +0000

    Fixes to the everything visitor.

--- trunk/src/pt/um/mrc/jobs/allmetrics/GrabEverythingVisitor.java
+++ trunk/src/pt/um/mrc/jobs/allmetrics/GrabEverythingVisitor.java
@@ -1,6 +1,7 @@
 package pt.um.mrc.jobs.allmetrics;
 
 import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.ImportDeclaration;
 import japa.parser.ast.body.BodyDeclaration;
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
 import japa.parser.ast.body.ConstructorDeclaration;
@@ -18,149 +19,189 @@ import pt.um.mrc.util.datatypes.IDType;
 import pt.um.mrc.util.io.visitors.GrabbingVisitor;
 
 //FIXME Acho que FUNCIONO
-public class GrabEverythingVisitor extends GrabbingVisitor<ElemID> {
+public class GrabEverythingVisitor extends GrabbingVisitor<ElemID>
+{
 
-	protected String fileName;
-	protected String packageName;
-	protected Map<ElemID, Text> elems = new TreeMap<ElemID, Text>();
+    protected String fileName;
+    protected String packageName;
+    protected Map<ElemID, Text> elems = new TreeMap<ElemID, Text>();
 
-	public GrabEverythingVisitor() {
-		super();
-	}
+    public GrabEverythingVisitor()
+    {
+        super();
+    }
 
-	public void init(String fName, String pkgName, Map<ElemID, Text> els) {
-		fileName = fName;
-		packageName = pkgName;
-		elems = els;
-	}
+    public void init(String fName, String pkgName, Map<ElemID, Text> els)
+    {
+        fileName = fName;
+        packageName = pkgName;
+        elems = els;
+    }
 
-	public class ClassVisitor extends VoidVisitorAdapter<Object> {
+    public class ClassVisitor extends VoidVisitorAdapter<Object>
+    {
 
-		public void visit(ClassOrInterfaceDeclaration c, Object arg) {
-			if (!c.isInterface()) {
-				ElemID aux = new ElemID();
-				StringBuilder sb = new StringBuilder();
+        public void visit(ClassOrInterfaceDeclaration c, Object arg)
+        {
+            if (!c.isInterface())
+            {
+                ElemID aux = new ElemID();
+                StringBuilder sb = new StringBuilder();
 
-				aux.setFileName(fileName);
-				aux.setPackageName(packageName);
-				aux.setClassName(c.getName());
-				aux.setIDType(IDType.CLASS);
+                aux.setFileName(fileName);
+                aux.setPackageName(packageName);
+                aux.setClassName(c.getName());
+                aux.setIDType(IDType.CLASS);
 
-				if (!c.getMembers().isEmpty()) {
-					for (BodyDeclaration td : c.getMembers()) {
-						td.setAnnotations(null);
-						td.setJavaDoc(null);
-						if (!(td instanceof MethodDeclaration)
-								&& !(td instanceof ConstructorDeclaration)) {
-							sb.append(td.toString());
-							sb.append("\n");
-						}
-					}
-				}
-				elems.put(aux, new Text(sb.toString()));
-			}
-		}
-	}
+                if (!c.getMembers().isEmpty())
+                {
+                    for (BodyDeclaration td : c.getMembers())
+                    {
+                        td.setAnnotations(null);
+                        td.setJavaDoc(null);
+                        if (!(td instanceof MethodDeclaration) && !(td instanceof ConstructorDeclaration))
+                        {
+                            sb.append(td.toString());
+                            sb.append("\n");
+                        }
+                    }
+                }
+                elems.put(aux, new Text(sb.toString()));
+            }
+        }
+    }
 
-	public class MethodVisitor extends VoidVisitorAdapter<Object> {
+    public class MethodVisitor extends VoidVisitorAdapter<Object>
+    {
+        public void visit(ClassOrInterfaceDeclaration c, Object arg)
+        {
+            if (!c.getMembers().isEmpty())
+            {
+                for (BodyDeclaration td : c.getMembers())
+                {
+                    ElemID aux = new ElemID("", c.getName(), fileName, packageName, IDType.METHOD, MetricType.NONE);
 
-		public void visit(ClassOrInterfaceDeclaration c, Object arg) {
+                    if (td instanceof MethodDeclaration)
+                    {
+                        buildEntry(aux, (MethodDeclaration) td);
+                    }
+                    if (td instanceof ConstructorDeclaration)
+                    {
+                        buildEntry(aux, (ConstructorDeclaration) td);
+                    }
+                }
+            }
+        }
 
-			if (!c.getMembers().isEmpty()) {
-				for (BodyDeclaration td : c.getMembers()) {
-					ElemID aux = new ElemID("", c.getName(), fileName,
-							packageName, IDType.METHOD, MetricType.NONE);
+        private void buildEntry(ElemID aux, MethodDeclaration md)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(md.getName());
 
-					if (td instanceof MethodDeclaration) {
-						buildEntry(aux, (MethodDeclaration) td);
-					}
-					if (td instanceof ConstructorDeclaration) {
-						buildEntry(aux, (ConstructorDeclaration) td);
-					}
-				}
-			}
-		}
+            if (md.getParameters() != null)
+                sb.append(md.getParameters().toString());
+            else
+                sb.append("[ ]");
 
-		private void buildEntry(ElemID aux, MethodDeclaration md) {
+            aux.setMethodName(sb.toString());
 
-			StringBuilder sb = new StringBuilder();
-			sb.append(md.getName());
+            md.setAnnotations(null);
+            md.setJavaDoc(null);
 
-			if (md.getParameters() != null)
-				sb.append(md.getParameters().toString());
-			else
-				sb.append("[ ]");
+            elems.put(aux, new Text(md.toString()));
+        }
 
-			aux.setMethodName(sb.toString());
+        private void buildEntry(ElemID aux, ConstructorDeclaration cd)
+        {
 
-			md.setAnnotations(null);
-			md.setJavaDoc(null);
+            StringBuilder sb = new StringBuilder();
+            sb.append(cd.getName());
 
-			elems.put(aux, new Text(md.toString()));
-		}
+            if (cd.getParameters() != null)
+                sb.append(cd.getParameters().toString());
+            else
+                sb.append("[ ]");
 
-		private void buildEntry(ElemID aux, ConstructorDeclaration cd) {
+            aux.setMethodName(sb.toString());
 
-			StringBuilder sb = new StringBuilder();
-			sb.append(cd.getName());
+            cd.setAnnotations(null);
+            cd.setJavaDoc(null);
 
-			if (cd.getParameters() != null)
-				sb.append(cd.getParameters().toString());
-			else
-				sb.append("[ ]");
+            elems.put(aux, new Text(cd.toString()));
+        }
+    }
 
-			aux.setMethodName(sb.toString());
 
-			cd.setAnnotations(null);
-			cd.setJavaDoc(null);
 
-			elems.put(aux, new Text(cd.toString()));
-		}
-	}
+    public void visit(CompilationUnit c, Object arg)
+    {
+        ElemID aux = new ElemID();
 
-	public void visit(CompilationUnit c, Object arg) {
-		ElemID aux = new ElemID();
+        aux.setIDType(IDType.FILE);
+        aux.setFileName(fileName);
+        aux.setPackageName(packageName);
 
-		aux.setIDType(IDType.FILE);
-		aux.setFileName(fileName);
-		aux.setPackageName(packageName);
+        StringBuilder sb = new StringBuilder();
 
-		StringBuilder sbFile = new StringBuilder();
+        if (c.getPackage() != null)
+        {
+            sb.append(c.getPackage().toString());
+            //sb.append("\n");
+        }
+        else
+        {
+            sb.append("<default>");
+            sb.append("\n");
+        }
 
-		if (c.getImports() != null)
-			sbFile.append(c.getImports().toString() + '\n');
+        if (c.getImports() != null)
+        {
+            for (ImportDeclaration id : c.getImports())
+            {
+                sb.append(id.toString());
+                sb.append("\n");
+            }
+        }
 
-		if (c.getPackage() != null)
-			sbFile.append(c.getPackage().toString() + '\n');
+        processTypes(c, sb);
+    }
+    
+    private void processTypes(CompilationUnit c, StringBuilder sb)
+    {
+        if (c.getTypes() != null)
+        {
+            for (TypeDeclaration t : c.getTypes())
+            {
+                if (t instanceof EnumDeclaration)
+                {
+                    t.setAnnotations(null);
+                    t.setJavaDoc(null);
+                    sb.append(t.toString());
+                    sb.append("\n");
+                }
 
-		else
-			sbFile.append("<default>\n");
-
-		for (TypeDeclaration t : c.getTypes()) {
-			t.setAnnotations(null);
-			t.setJavaDoc(null);
-
-			if (!t.getMembers().isEmpty()) {
-				if (t instanceof EnumDeclaration) {
-					for (BodyDeclaration b : t.getMembers()) {
-						b.setAnnotations(null);
-						b.setJavaDoc(null);
-					}
-					sbFile.append(t.toString());
-				}
-
-				if (t instanceof ClassOrInterfaceDeclaration) {
-					if (((ClassOrInterfaceDeclaration) t).isInterface()) {
-						for (BodyDeclaration b : t.getMembers()) {
-							b.setAnnotations(null);
-							b.setJavaDoc(null);
-						}
-						sbFile.append(t.toString());
-					}
-				}
-			}
-			elems.put(aux, new Text(sbFile.toString()));
-		}
-	}
-
+                if (t instanceof ClassOrInterfaceDeclaration)
+                {
+                    if (((ClassOrInterfaceDeclaration) t).isInterface())
+                    {
+                        for (BodyDeclaration b : t.getMembers())
+                        {
+                            b.setAnnotations(null);
+                            b.setJavaDoc(null);
+                        }
+                        t.setAnnotations(null);
+                        t.setJavaDoc(null);
+                        sb.append(t.toString());
+                    }
+                }
+            }
+        }
+        ElemID aux = new ElemID();
+        
+        aux.setFileName(fileName);
+        aux.setPackageName(packageName);
+        aux.setIDType(IDType.FILE);
+        
+        elems.put(aux, new Text(sb.toString()));
+    }
 }
