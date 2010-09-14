f8b558bd006dfca86fa68623d66203737fd3d0f6
ti.veloso <ti.veloso@abb60ae5-bb80-0197-2736-cdeed78a909b>
2010-09-14 09:18:39 +0000

    AllMetrics Reducer

--- trunk/src/pt/um/mrc/jobs/allmetrics/AllMetricsReducer.java
+++ trunk/src/pt/um/mrc/jobs/allmetrics/AllMetricsReducer.java
@@ -1,8 +1,28 @@
 package pt.um.mrc.jobs.allmetrics;
 
-import pt.um.mrc.util.reducers.IdentityReducer;
+import java.io.IOException;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class AllMetricsReducer extends IdentityReducer<ElemID, MetricValue, ElemID, MetricValue>{
-
+public class AllMetricsReducer extends Reducer<ElemID, MetricValue, ElemID, Text>
+{
+    @Override
+    protected void reduce(ElemID key, Iterable<MetricValue> value, Context context) throws IOException, InterruptedException
+    {
+        Long aux = 0L;
+        
+        for (MetricValue metricValue : value)
+        {
+            if (metricValue.getIsText())
+            {
+                context.write(key, new Text(metricValue.getText()));
+            } else 
+            {
+                aux += metricValue.getIntw();
+            }
+        }
+        
+        context.write(key, new Text(aux.toString()));
+    }
 }
